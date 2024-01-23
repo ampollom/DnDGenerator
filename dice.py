e8a3ac5b6e93189c899757143bbd815af5cdd63f
Amy <amy.pollom@gmail.com>
2024-01-23 03:25:39 -0800

    Added an option to role for names

--- dice.py
+++ dice.py
@@ -4,6 +4,8 @@ import pandas as pd
 import pyarrow
 
 arr = []
+name_num = 1006
+item_num = 21
 
 #checks if input is an integer
 def is_integer(n):
@@ -12,27 +14,39 @@ def is_integer(n):
 #generates a psuedo-random number between 1 and 20 and puts it in an array
 def roll_dice(input_string):
 
+    filename = ""
     if is_integer(input_string) != True:
         print("Is not a digit")
         roll_dice()
     else:
-
-        while input_string != 0:
-            dice_value = random.randrange(1, 21)
-            arr.append(dice_value)
-            input_string = input_string - 1
+        pickasheet = input("What are you rolling for? Names or Items?: ").lower()
+        if (pickasheet == 'names'):
+            while input_string != 0:
+                dice_value = random.randrange(1, name_num)
+                arr.append(dice_value)
+                input_string = input_string - 1
+                filename = "names.xlsx"
+        else:
+            while input_string != 0:
+                dice_value = random.randrange(1, item_num)
+                arr.append(dice_value)
+                input_string = input_string - 1
+                filename = "gold.xlsx"
     #print(arr)
-    read_spreadsheet(arr)
+    read_spreadsheet(arr, filename)
 
 #reads and pulls items from spreadsheet
-def read_spreadsheet(arr):
+def read_spreadsheet(arr, filename):
 
-    df = pd.read_excel('gold.xlsx')
+    df = pd.read_excel(filename)
     items = []
+
+    print("You can choose from the following options: girl_names, boy_names, spell_scrolls, gold"
+          "random_items, weapons, or food.")
 
     while len(arr) != 0:
         num = arr[0]
-        column_input = input("What are you looking for? gold, weapons, random_items, food, or spell_scrolls: ")
+        column_input = input("What are you looking for?: ")
         s = df.loc[num-1, column_input]
         items.append(s)
         del arr[0]
@@ -52,7 +66,7 @@ def print_items(items):
 
 def reroll_dice():
     s = input("Would you like to reroll [y/N]: ")
-    s.lower().isalpha()
+    s = s.lower()
 
     if s == 'y':
         run_program()
