10153634d17123c7fcc81494dd1cf48027739522
Amy <amy.pollom@gmail.com>
2024-01-27 22:18:29 -0800

    Created a GUI for program

--- dice.py
+++ dice.py
@@ -1,4 +1,3 @@
-
 import random
 import pandas as pd
 import pyarrow
@@ -12,61 +11,68 @@ def is_integer(n):
     return isinstance(n,int)
 
 #generates a psuedo-random number between 1 and 20 and puts it in an array
-def roll_dice(input_string):
+def roll_dice(input_string, filename, dict):
 
-    filename = ""
+    #filename = ""
     if is_integer(input_string) != True:
         print("Is not a digit")
         roll_dice()
     else:
-        pickasheet = input("What are you rolling for? Names or Items?: ").lower()
-        if (pickasheet == 'names'):
-            filename = "names.xlsx"
-            print("You can choose from the following options: girl_names or boy_names")
+        #pickasheet = input("What are you rolling for? Names or Items?: ").lower()
+        if (filename == 'names.xlsx'):
+            #filename = "names.xlsx"
             while input_string != 0:
                 dice_value = random.randrange(1, name_num)
                 arr.append(dice_value)
                 input_string = input_string - 1
 
-        else:
-            filename = "gold.xlsx"
-            print("You can choose from the following options: gold, spell_scrolls, weapons_shitty"
-                  ", random_shitty_items, random_nice_items")
+        elif (filename == "gold.xlsx"):
             while input_string != 0:
                 dice_value = random.randrange(1, item_num)
                 arr.append(dice_value)
                 input_string = input_string - 1
 
     #print(arr)
-    read_spreadsheet(arr, filename)
+    read_spreadsheet(arr, filename, dict)
+    #return arr
 
 
 
 #reads and pulls items from spreadsheet
-def read_spreadsheet(arr, filename):
+def read_spreadsheet(arr, filename, dict):
 
     df = pd.read_excel(filename)
     items = []
+    dict_keys =dict.keys()
+    dict_values = dict.values()
+    #print(dict_keys)
+    #print(dict_values)
 
     while len(arr) != 0:
-        num = arr[0]
-        column_input = input("What are you looking for?: ")
-        s = df.loc[num-1, column_input]
-        items.append(s)
-        del arr[0]
+        for (i,j) in zip(dict_keys, dict_values):
+            #print(i)
+            #print(j)
+            key= i
+            counter = j
+            #print(counter)
+            while counter != 0:
+                num = arr[0]
+                s = df.loc[num-1,key]
+                #print(s)
+                items.append(s)
+                counter = counter-1
+                del arr[0]
     print_items(items)
+    #return items
 
 #prints items
 def print_items(items):
-
-    print()
-    print("***********************************")
-    print("Found following items:")
-    for i in items:
-        print(i)
-    print("***********************************")
-    print()
-    reroll_dice()
+    #print("***********************************\n"+
+         # "Found following items:\n")
+    for item in items:
+        print(item)
+    #print("***********************************\n")
+    #reroll_dice()
 
 def reroll_dice():
     s = input("Would you like to reroll [y/N]: ")
@@ -82,7 +88,12 @@ def reroll_dice():
         reroll_dice()
 
 
-def run_program():
-    num_of_dice = int(input("How many dice do you want to roll?: "))
-    roll_dice(num_of_dice)
+#def run_program():
+    #num_of_dice = int(input("How many dice do you want to roll?: "))
+    #roll_dice(num_of_dice)
 
+
+
+
+
+#sg.Button('Ok'), sg.Button('Cancel')]
--- main.py
+++ main.py
@@ -1,3 +1,109 @@
+from PySimpleGUI import HorizontalSeparator
 import dice
+import PySimpleGUI as sg
+import os
+from PIL import Image, ImageTk
+import io
 
-dice.run_program()
+
+#sg.preview_all_look_and_feel_themes()
+#GUI Theme
+sg.theme('darkgreen1')
+
+#SPIN Counter
+num_of_dice = [i for i in range(1, 21)]
+dicenum = sg.Spin(num_of_dice, initial_value=1, readonly=True,  size=5, enable_events=True, key='-DICE-')
+
+#Layout of Initial GUI
+layout = [[sg.Text("Choose your generator:")],
+           [sg.Radio("Names","RADIO1", enable_events=True, key='R1', default=False),
+            sg.Radio("Loot","RADIO1", enable_events=True, key='R2', default=False)],
+            [sg.HorizontalSeparator(color = None)],
+          [sg.Text("How many dice are you rolling?"), dicenum],
+            [sg.HorizontalSeparator(color = None)],
+            [sg.Checkbox("Girl Names", default=False, enable_events=True, key="gnames"),
+             sg.Checkbox("Boy Names", default=False, enable_events=True, key="bnames")],
+             [sg.Checkbox("Gold", default=False, enable_events=True, key="gold"),
+             sg.Checkbox("Spell Scrolls", default=False, enable_events=True, key="sscrolls"),
+             sg.Checkbox("Food", default=False, enable_events=True, key="food"),
+             sg.Checkbox("Random Items", default=False, enable_events=True, key="ritems"),
+             sg.Checkbox("Weapons", default=False, enable_events=True, key="weapons"),
+             sg.Checkbox("Nice Items", default=False, enable_events=True, key="nitems")],
+            [sg.HorizontalSeparator(color = None)],
+          [sg.Text("Here is what we found:")],[sg.Output(size=[20,5])],
+          [sg.Button("OK"), sg.Button("Reroll"), sg.Button("Exit")],],
+
+#GUI Window
+
+window = sg.Window("Dungeons and Dragons Generator", layout)
+
+
+#Events
+while True:
+    event, values = window.read()
+    if event in (sg.WINDOW_CLOSED, "Exit"):
+        break
+    if event == "OK":
+        filename = ''
+        dicerolled = int(values['-DICE-'])
+        if values['R1'] == True:
+            filename = "names.xlsx"
+            girl_names = 0
+            boy_names = 0
+
+            if values['gnames'] == True:
+                girl_names = int(sg.popup_get_text("How Many Girls?: "))
+            if values['bnames'] == True:
+                boy_names = int(sg.popup_get_text("How Many Boys? "))
+            name_dict = {"girl_names":girl_names,
+                    "boy_names":boy_names}
+            #print(name_list)
+            dice.roll_dice(dicerolled, filename, name_dict)
+
+        elif values['R2'] == True:
+            filename = "gold.xlsx"
+            gold = 0
+            sscrolls = 0
+            food = 0
+            weapons = 0
+            ritems = 0
+            nitems = 0
+            if values["gold"] == True:
+                gold = int(sg.popup_get_text("How much gold?"))
+            if values["sscrolls"] == True:
+                sscrolls = int(sg.popup_get_text("How many Spell Scrolls?"))
+            if values["food"] == True:
+                food = int(sg.popup_get_text("How many Spell Scrolls?"))
+            if values["weapons"] == True:
+                weapons = int(sg.popup_get_text("How many weapons?"))
+            if values["ritems"] == True:
+                ritems = int(sg.popup_get_text("How many random items?"))
+            if values["nitems"] == True:
+                nitems = int(sg.popup_get_text("How many nice items?"))
+            items_dict = {"gold": gold,
+                          "spell_scrolls": sscrolls,
+                          "food": food,
+                          "weapons_shitty":weapons,
+                          "random_items_shitty": ritems,
+                          "nice_items": nitems}
+            #print(items_dict)
+            items = dice.roll_dice(dicerolled, filename, items_dict)
+            #for item in items:
+                #print(item)
+            sg.popup_yes_no("Do you want to reroll?", title="Reroll?")
+
+
+        #print(filename)
+        #print(dicerolled)
+        #dice.roll_dice(dicerolled, filename)
+
+
+
+
+window.close()
+
+
+
+
+
+#dice.run_program()
